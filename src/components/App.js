912e835f98c5c08f55f90a1f048d3a49aa4347f9
Svyatoslav <sheinsvyatoslav@gmail.com>
2022-06-11 23:30:41 +0300

    bugs fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
+import React, { useState, useEffect } from 'react';
 import Main from "./Main";
+import {flightsInitial} from '../utils/flightsAll'
 
 export default function App() {
+  const [flights, setFlights] = useState(flightsInitial)
+
+  useEffect(() => {
+    localStorage.setItem('flights', JSON.stringify(flightsInitial))
+  }, []);
   
   return (
-    <Main />
+    <Main flights={flights} setFlights={setFlights}/>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,12 @@
-import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import FlightsItemList from './FlightsItemList'
-import {flightsInitial} from '../utils/flightsAll'
 
-export default function Main() {
-  const [flights, setFlights] = useState(flightsInitial)
-
-  useEffect(() => {
-    localStorage.setItem('flights', JSON.stringify(flightsInitial))
-  }, []);
-
+export default function Main(props) {
+  
   return (
     <main className="content">
-      <Sidebar flights={flights} setFlights={setFlights}/>
-      <FlightsItemList flights={flights}/>
+      <Sidebar flights={props.flights} setFlights={props.setFlights}/>
+      <FlightsItemList flights={props.flights}/>
     </main>
   );
 }
